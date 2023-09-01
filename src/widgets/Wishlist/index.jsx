525ac7b103bbd06c59cd5814b18374a015e4a406
lDaretl <mr.artoym@mail.ru>
2023-09-01 19:35:42 +0300

    fix for displaying date

--- src/widgets/Wishlist/index.jsx
+++ src/widgets/Wishlist/index.jsx
@@ -16,7 +16,7 @@ const updatingDateTime = (timestamp) => {
     const date = new Date(timestamp)
     return {
         time: `${date.getHours()}:${date.getMinutes().toString().padStart(2, 0)}:${date.getSeconds().toString().padStart(2, 0)}`,
-        date: `${date.getMonth()}.${date.getDate()}.${date.getFullYear()}`
+        date: `${date.getMonth() + 1}.${date.getDate()}.${date.getFullYear()}`
     }
 }
 
